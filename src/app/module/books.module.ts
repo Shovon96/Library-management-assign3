a91b7abdfd3e0df06d52386a85cbf8a11efdab41
Shovon96 <fakhruddinshovon@gmail.com>
2025-06-23 03:10:44 +0600

    create books interface and modules

---
+++ src/app/module/books.module.ts
@@ -0,0 +1,55 @@
+import { model, Schema } from "mongoose";
+import { IBooks } from "../interface/books.interface";
+
+const booksSchema = new Schema<IBooks>(
+    {
+        title: {
+            type: String,
+            required: [true, 'The book Title is required'],
+            trim: true
+        },
+        author: {
+            type: String,
+            required: [true, 'The Author name is required'],
+            trim: true
+        },
+        genre: {
+            type: String,
+            required: [true, 'The Genre is required'],
+            enum: {
+                values: ["FICTION", "NON_FICTION", "SCIENCE", "HISTORY", "BIOGRAPHY", "FANTASY"],
+                message:
+                    "The Genre must be choose of: FICTION, NON_FICTION, SCIENCE, HISTORY, BIOGRAPHY, FANTASY",
+            },
+            uppercase: true,
+            trim: true
+        },
+        isbn: {
+            type: String,
+            required: [true, 'The ISBN is required'],
+            unique: [true, "The ISBN number need to unique"],
+            trim: true
+        },
+        description: {
+            type: String,
+            trim: true
+        },
+        copies: {
+            type: Number,
+            required: [true, "The book Copies feild is required"],
+            min: [0, "Not allowed the negative numbers"],
+            trim: true
+        },
+        available: {
+            type: Boolean,
+            default: true
+        }
+    },
+    {
+        versionKey: false,
+        timestamps: true
+    }
+)
+
+
+const Books = model("Book", booksSchema);
